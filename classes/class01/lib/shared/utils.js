3bbed41988d4cf26adfc342c6d39ca16f0f219b4
Pedro Henrique Bergental <phbergental@gmai.com>
2023-03-05 10:19:39 -0300

    feat: play and pause video with blink gesture

---
+++ classes/class01/lib/shared/utils.js
@@ -0,0 +1,28 @@
+function supportWorkerTypes() {
+  let supports = false
+  let tester = {
+    get type() { supports = true }
+  }
+  try {
+    new Worker('blob://', tester).terminate()
+  } finally {
+    return supports
+  }
+}
+// Design Pattern - Closure
+function prepareRunChecker({ timerDelay }) {
+  let lastEvent = Date.now()
+  return {
+    shouldRun() {
+      const result = (Date.now() - lastEvent) > timerDelay
+      if (result) lastEvent = Date.now()
+
+      return result
+    }
+  }
+}
+
+export {
+  supportWorkerTypes,
+  prepareRunChecker
+}
